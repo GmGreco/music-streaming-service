a70dd8ba3b21304f82df8fa0ae20d968d5621c2d
GmGreco <105087288+GmGreco@users.noreply.github.com>
2023-07-14 18:55:44 -0300

    initial commit

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Streaming Service",
+  description: "Escute suas músicas favoritas",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
